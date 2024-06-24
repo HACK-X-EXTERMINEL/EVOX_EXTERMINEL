881078bed6352d71ea6fb2ec84e9d1405362ed51
Hack-x EXTERMINEL <156604635+HACK-X-EXTERMINEL@users.noreply.github.com>
2024-06-24 15:02:24 +0100

    Add files via upload

---
+++ login/branch/js/LOAD.js
@@ -0,0 +1,54 @@
+document.addEventListener('contextmenu', function(e) {
+  if (e.target.nodeName === 'IMG') {
+    e.preventDefault();
+  }
+}, false);
+
+// منع السحب والإفلات للصور
+document.addEventListener('dragstart', function(e) {
+  if (e.target.nodeName === 'IMG') {
+    e.preventDefault();
+  }
+}, false);
+window.addEventListener('touchstart', function(e) {
+  if (e.target.nodeName === 'IMG') {
+    e.preventDefault();
+  }
+}, false);
+var loader = document.getElementById("preload");
+
+// تعطيل التمرير
+function disableScroll() {
+  // حفظ موقف التمرير الحالي
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  // تعطيل التمرير
+  window.onscroll = function() {
+    window.scrollTo(scrollLeft, scrollTop);
+  };
+}
+
+// تمكين التمرير
+function enableScroll() {
+  window.onscroll = null;
+}
+
+// تعطيل التمرير عند بدء تحميل الصفحة
+disableScroll();
+
+window.addEventListener("load", function() {
+  var fadeEffect = setInterval(function () {
+    if (!loader.style.opacity) {
+      loader.style.opacity = 1;
+    }
+    if (loader.style.opacity > 0) {
+      loader.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeEffect);
+      // تمكين التمرير بعد إخفاء الـ loader
+      loader.style.display = "none";
+      enableScroll();
+    }
+  }, 100);
+});
